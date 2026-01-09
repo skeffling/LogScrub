715515c3aafc85c1891b7e4557ac8f7ba528a99d
andrew <git@andrewhearn.com>
2026-01-09 18:16:50 +0000

    Add PII double-check reminder to getting started instructions

--- packages/web/src/components/Editor.tsx
+++ packages/web/src/components/Editor.tsx
@@ -899,6 +899,7 @@ export const Editor = forwardRef<EditorHandle, EditorProps>(function Editor({ in
                   <p>3. Enable/disable rules in Detection Rules panel</p>
                   <p>4. Click <span className="text-blue-500 dark:text-blue-400">Scrub</span> to apply replacements</p>
                   <p>5. Use diff view to review changes, then Copy or Download</p>
+                  <p className="text-yellow-600 dark:text-yellow-500 mt-2">⚠ Always double-check for PII before sharing the file!</p>
                 </div>
               </div>
             )}
